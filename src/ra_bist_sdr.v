bab3683814ca44427fb1eb8129687159cb554fc5
Peter Tikalsky <20569264+Peter-Tik@users.noreply.github.com>
2021-11-12 13:18:59 -0600

    outlined BIST

--- src/ra_bist_sdr.v
+++ src/ra_bist_sdr.v
@@ -109,7 +109,47 @@ module ra_bist_sdr (
    assign seq_d = seq_q;
    assign active = seq_q != 6'h3F;
    assign status = 0;
-    //outline for BIST
+   /*
+   Outline for BIST
+   ----------------------------------------------------------------------
+   first off, how I think this thing is supposed to work is that we need a
+   final flag signifying the BIST is successfully ran, and one where it fa-
+   ils.
+   uhhhhhh something's gotta happen here
+   like:
+   enable write data
+   assign all 0s to addr 0x00-0x3F (using signals wr0_adr_in &
+   wr0_dat_in)
+
+   enable read data (rd0_enb_in)
+
+   read addr 0x00-0x3f one at a time (rd0_adr_in/out and rd0_dat)
+
+   after each read, write all 1s to each addr 0x00-0x3F
+   ^^this happens after each read, so like, read data at 0x00, write all
+   ones to 0x00, step forward to next address, 0x01 (process A)
+
+   for each valid read of all 0s, save output in a 6-bit bus that counts
+   up for each valid read or something
+
+   now, step through the exact same read/write process but replacing
+   all 1s with all 0s.
+
+   read all 0s through same process (NO WRITE CHANGE THIS TIME)
+
+   Now, write all 1s to each address 0x3F-0x00.
+   repeat the process A, reading data at each address,replacing all 1s
+   with all 0s for each address 0x3F-0x00, and keeping track of whether
+   working or not.
+
+   finally, read all 0s through same process (NO WRITE HERE EITHER)
+   at the end, there's gotta be some kinda comparison where you check
+   that the tests were valid for both ascending and descending runs.
+
+   if both are valid, flag BIST_PASSED. if one of the runs is invalid,
+   flag BIST_FAIL_UP, or BIST_FAIL_DOWN or both.
+
+   */
 
 
    // outputs
@@ -120,5 +160,6 @@ module ra_bist_sdr (
    assign wr0_enb_out = (active) ? bist_wr0_enb : wr0_enb_in;
    assign wr0_adr_out = (active) ? bist_wr0_adr : wr0_adr_in;
    assign wr0_dat_out = (active) ? bist_wr0_dat : wr0_dat_in;
+   //assign rd0_dat = (active) ? haven't done anything here yet
 
 endmodule
